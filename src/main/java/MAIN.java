77f205a6f23cafe40c56da55cafbb63bdc3aece9
mokesh22-ops <121173652+mokesh22-ops@users.noreply.github.com>
2022-12-22 19:25:37 +0530

    new program1

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,5 +1,6 @@
 public class MAIN {
     public static void main (String[] args) {
-        System.out.println("Mukesh is the best guy");
+        System.out.println("Mokesh the best guy");
+        System.out.println("this is myworld");
     }
 }
